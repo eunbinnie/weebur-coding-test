9125dc5f1747db1f91d1a41a38eded63e3d33aa0
이은빈 <110139747+eunbinnie@users.noreply.github.com>
2025-05-30 23:02:09 +0900

    feat: 상품 API 함수 및 파라미터 직렬화 유틸 함수 추가 (#2)
    * feat: 상품 리스트 조회 API 함수 구현
    
    * feat: 상품 생성 API 함수 구현

---
+++ src/lib/api/products.ts
@@ -0,0 +1,30 @@
+import {
+  AddProductRequestBody,
+  ProductListResponse,
+  ProductsParams,
+} from '@/types/products.types';
+import ky from 'ky';
+import { serializeParams } from '../utils/api';
+
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+// 상품 리스트 조회
+export const getProducts = async (params: ProductsParams) => {
+  const searchParams = serializeParams(params);
+  const res = await ky
+    .get<ProductListResponse>(`${BASE_URL}/products`, {
+      searchParams,
+    })
+    .json();
+  return res;
+};
+
+// 상품 생성
+export const createProduct = async (body: AddProductRequestBody) => {
+  const res = await ky
+    .post(`${BASE_URL}/products/add`, {
+      json: body,
+    })
+    .json();
+  return res;
+};
---
+++ src/types/products.types.ts
@@ -0,0 +1,67 @@
+// 상품 리스트 조회 파라미터
+export interface ProductsParams {
+  limit?: number;
+}
+
+// 상품 리스트 조회 응답
+export interface ProductListResponse {
+  products: Product[];
+  total: number;
+  skip: number;
+  limit: number;
+}
+
+export interface Product {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  tags: string[];
+  brand: string;
+  sku: string;
+  weight: number;
+  dimensions: Dimensions;
+  warrantyInformation: string;
+  shippingInformation: string;
+  availabilityStatus: string;
+  reviews: Review[];
+  returnPolicy: string;
+  minimumOrderQuantity: number;
+  meta: ProductMeta;
+  thumbnail: string;
+  images: string[];
+}
+
+export interface Dimensions {
+  width: number;
+  height: number;
+  depth: number;
+}
+
+export interface Review {
+  rating: number;
+  comment: string;
+  date: string; // ISO date string
+  reviewerName: string;
+  reviewerEmail: string;
+}
+
+export interface ProductMeta {
+  createdAt: string; // ISO date string
+  updatedAt: string; // ISO date string
+  barcode: string;
+  qrCode: string;
+}
+
+// 상품 생성 파라미터
+export interface AddProductRequestBody {
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage?: number;
+  brand: 'Apple' | 'Samsung' | 'Weebur';
+}
