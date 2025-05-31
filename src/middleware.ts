a39251a52a7c8340a94b9a483a5a561c0c4237d8
eunbinnie <dmsqlsdl54@gmail.com>
2025-05-31 21:01:14 +0900

    feat: 랜덤 view 타입 쿠키 설정 미들웨어 추가 (24시간 유지)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,49 @@
+'use server';
+
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-// /에 접근 시 /products로 리다이렉트
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/products', request.url));
+  const pathname = request.nextUrl.pathname;
+  const viewCookie = request.cookies.get('view');
+  const random = Math.random() > 0.5 ? 'list' : 'grid';
+
+  const cookieOptions = {
+    maxAge: 60 * 60 * 24,
+    path: '/',
+  };
+
+  if (!viewCookie) {
+    request.cookies.set('view', random);
+  }
+
+  if (pathname === '/') {
+    const res = NextResponse.redirect(new URL('/products', request.url), {
+      headers: new Headers(request.headers),
+    });
+
+    if (!viewCookie) {
+      res.cookies.set('view', random, cookieOptions);
+    }
+
+    return res;
+  } else {
+    const res = NextResponse.next({
+      request: {
+        headers: new Headers(request.headers),
+      },
+    });
+
+    if (!viewCookie) {
+      res.cookies.set('view', random, cookieOptions);
+    }
+
+    return res;
+  }
 }
 
 export const config = {
-  matcher: '/',
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|icons|fonts|images).*)',
+  ],
 };
