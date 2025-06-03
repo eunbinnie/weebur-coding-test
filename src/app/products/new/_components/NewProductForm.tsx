fc7135825739517188b3045380c4cfb8628ee9b0
eunbinnie <dmsqlsdl54@gmail.com>
2025-06-03 12:54:38 +0900

    feat: 새 상품 등록 폼 세팅

--- src/app/products/new/_components/NewProductForm.tsx
+++ src/app/products/new/_components/NewProductForm.tsx
@@ -1,18 +1,41 @@
+import type { SubmitHandler } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+
+import type { AddProductRequestBody } from '@/types/products.types';
+
+import Button from '@/components/Button';
 import Input from '@/components/Input';
 import InputWrapper from '@/components/InputWrapper';
 
 const NewProductForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<AddProductRequestBody>({ mode: 'onChange' });
+  const onSubmit: SubmitHandler<AddProductRequestBody> = (data) => {
+    console.log(data);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* 상품명 */}
       <InputWrapper
         label='상품명'
         htmlFor='title'
         required
-        error
-        errorMessage='에러 메세지'
+        error={!!errors.title}
+        errorMessage={errors.title?.message}
       >
-        <Input id='title' placeholder='상품명' />
+        <Input
+          id='title'
+          placeholder='상품명을 입력하세요'
+          {...register('title')}
+        />
       </InputWrapper>
+      <Button type='submit' className='mt-5'>
+        등록하기
+      </Button>
     </form>
   );
 };
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ import { forwardRef } from 'react';
 import { cn } from '@/lib/utils';
 
 interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  customClassName?: string;
+  className?: string;
 }
 
 /**
@@ -12,14 +12,14 @@ interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
  * @param customClassName tailwind className을 확장할 수 있습니다.
  */
 const Button = forwardRef<HTMLButtonElement, IButtonProps>(
-  ({ type = 'button', children, customClassName, ...rest }, ref) => {
+  ({ type = 'button', children, className, ...rest }, ref) => {
     return (
       <button
         ref={ref}
         type={type}
         className={cn(
           'h-[30px] rounded bg-black px-4 text-sm text-white',
-          customClassName,
+          className,
         )}
         {...rest}
       >
