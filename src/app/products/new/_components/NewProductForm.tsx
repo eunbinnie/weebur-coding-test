2f8b775f99d3dfb9800aa275c5d6d243af662ced
eunbinnie <dmsqlsdl54@gmail.com>
2025-06-03 20:09:55 +0900

    feat: 할인 적용된 최종 가격 폼 하단에 렌더링

--- src/app/products/new/_components/NewProductForm.tsx
+++ src/app/products/new/_components/NewProductForm.tsx
@@ -7,6 +7,8 @@ import type { AddProductInput } from '@/schemas/product.schema';
 import { addProductSchema } from '@/schemas/product.schema';
 import { zodResolver } from '@hookform/resolvers/zod';
 
+import { formatNumberWithCommas } from '@/lib/utils/number';
+
 import type { AddProductRequestBody } from '@/types/products.types';
 
 import Button from '@/components/Button';
@@ -33,11 +35,18 @@ const NewProductForm = () => {
   const {
     register,
     handleSubmit,
+    watch,
     formState: { errors, isValid },
   } = useForm<AddProductInput>({
     mode: 'onChange',
     resolver: zodResolver(addProductSchema),
   });
+
+  const price = watch('price') || 0; // 상품 가격
+  const discountPercentage = watch('discountPercentage') || 0; // 할인율
+
+  const finalPrice = price - (price * (discountPercentage || 0)) / 100; // 할인 적용된 최종 가격
+
   const onSubmit: SubmitHandler<AddProductInput> = (data) => {
     const formData = { ...data };
     // description 필드가 비어있으면 제거
@@ -99,7 +108,14 @@ const NewProductForm = () => {
           </SelectContent>
         </Select>
       </InputWrapper> */}
-      <div className='flex pt-5'>
+      <div className='flex items-center justify-between pt-5'>
+        <p className='text-sm'>
+          최종 가격:{' '}
+          <span className='font-bold'>
+            {formatNumberWithCommas(finalPrice.toString())}
+          </span>
+          원
+        </p>
         <Button type='submit' disabled={!isValid} className='ml-auto'>
           등록하기
         </Button>
