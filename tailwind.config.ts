d7060dd97aeab3fafdc6c80382c07e1a97a881a7
eunbinnie <dmsqlsdl54@gmail.com>
2025-06-04 14:09:37 +0900

    feat: 페이지 상단 이동 버튼 컴포넌트 구현

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -67,6 +67,32 @@ const config: Config = {
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
       },
+      keyframes: {
+        'fade-up': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(5px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+        'fade-down': {
+          '0%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+          '100%': {
+            opacity: '0',
+            transform: 'translateY(5px)',
+          },
+        },
+      },
+      animation: {
+        'fade-up': 'fade-up 0.5s ease-in-out forwards',
+        'fade-down': 'fade-down 0.5s ease-in-out forwards',
+      },
     },
   },
   plugins: [tailwindcssAnimate],
