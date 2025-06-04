f693e1c6ccd19baeab143a230874185685e60be5
eunbinnie <dmsqlsdl54@gmail.com>
2025-06-04 10:36:48 +0900

    feat: 상품 리스트 useInfiniteQuery로 변경

--- src/app/products/_components/ProductList.tsx
+++ src/app/products/_components/ProductList.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 
 import { getProducts } from '@/lib/api/products';
 import { cn } from '@/lib/utils';
 
 import type { ProductView } from '@/types/products.types';
+
+import Button from '@/components/Button';
 
 import { PRODUCTS_PER_PAGE, PRODUCTS_SELECT } from '../_constants/products';
 
@@ -19,18 +21,26 @@ interface IProductList {
  * 상품 목록을 렌더링하는 컴포넌트입니다.
  *
  * 상품 리스트 데이터를 불러와 리스트 또는 그리드 형태로 렌더링합니다.
- *
- * @TODO infinite scroll 기능 구현현
  */
 const ProductList = ({ view }: IProductList) => {
-  const { data } = useQuery({
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery({
     queryKey: ['products'],
-    queryFn: () =>
+    queryFn: ({ pageParam = 0 }) =>
       getProducts({
         limit: PRODUCTS_PER_PAGE,
-        skip: 0,
+        skip: pageParam,
         select: PRODUCTS_SELECT,
       }),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => lastPage.skip + PRODUCTS_PER_PAGE,
   });
 
   const listClassName = '[&>*:not(:first-child)]:mt-5'; // 리스트형 클래스네임
@@ -38,13 +48,24 @@ const ProductList = ({ view }: IProductList) => {
     'grid grid-cols-1 xs:grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4'; // 그리드형 클래스네임
 
   return (
-    <section
-      className={cn('mt-8', view === 'list' ? listClassName : gridClassName)}
-    >
-      {data?.products.map((item) => (
-        <ProductListItem key={item.id} item={item} view={view} />
-      ))}
-    </section>
+    <>
+      <section
+        className={cn('mt-8', view === 'list' ? listClassName : gridClassName)}
+      >
+        {data?.pages.map((page) =>
+          page.products.map((item) => (
+            <ProductListItem key={item.id} item={item} view={view} />
+          )),
+        )}
+      </section>
+      <Button
+        className='mt-8'
+        disabled={!hasNextPage || isFetchingNextPage}
+        onClick={() => fetchNextPage()}
+      >
+        더 보기
+      </Button>
+    </>
   );
 };
 
